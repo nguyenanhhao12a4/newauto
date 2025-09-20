94d211af53191c64140493a1f02e755fb0d37563
nguyenanhhao12a4 <nguyenanhhao12a4@gmail.com>
2025-09-20 21:04:35 +0700

    chinh sua code 2

--- base/base_test.py
+++ base/base_test.py
@@ -3,21 +3,22 @@ from selenium import webdriver
 from time import sleep
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
+from utils.config_reader import ConfigReader
 class BaseTest:
     @pytest.fixture(scope="class", autouse=True)
     def setup(self, request):
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
         driver.maximize_window()
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
-        #xac nhan trang login hien thi
-        login_page = LoginPage(driver)
-        assert login_page.is_login_page_displayed(), "Login page is not displayed"
-        print("Login page is displayed")
+        
+        base_url = ConfigReader.get_base_url()
+        if base_url:
+            driver.get(base_url)
+        
+        
 
         request.cls.driver = driver
 
